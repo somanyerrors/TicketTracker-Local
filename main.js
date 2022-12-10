46def71412153d160ab3314cc3c5eb88d1dab572
ShiftingDitto <98179572+ShiftingDitto@users.noreply.github.com>
2022-12-10 09:56:19 -0500

    close issue and delete issue added

--- main.js
+++ main.js
@@ -17,14 +17,16 @@ function fetchIssues() {
         let status = issues[i].status
         let statusColor = status == "Closed" ? 'label-success' : 'label-info'
 
-        issuesList.innerHTML +=
+        issuesList.innerHTML += 
         '<div class="well">' +
         '<h6>Issue ID:' + id + '</h6>' +
-        '<p><span class= "label ' +statusColor + ' ">' + status + 
-        '</span></p>' +
-        '<h3>' + subject + '</h3>'
-        '<p>' + description + '</p>' +
-        '</div>'
+        '<p><span class= "label ' + statusColor + ' ">' + status + '</span></p>' +
+        '<h3>' + subject + '</h3>' +
+        '<p>' + description + '</p>' + 
+        '<p><span class="glyphicon glyphicon-time"></span> ' + severity + ' ' + '<span class="glyphicon glyphicon-user"></span>' + assignedTo + '</p>' +
+        '<a href="#" class="btn btn-warning" onclick="setStatusClosed(\''+id+'\')">Close</a> ' +
+        '<a href="#" class="btn btn-danger" onclick="deleteIssue(\''+id+'\')">Delete</a> '
+        + '</div>'
     }
 }
 
@@ -61,4 +63,31 @@ function saveIssue(e) {
     fetchIssues()
 
     e.preventDefault()
+}
+
+function setStatusClosed(id) {
+    let issues = JSON.parse(localStorage.getItem('issues'))
+    for(let i=0; i < issues.length; i++) {
+        if(issues[i].id === id) {
+            issues[i].status = "Closed"
+        }
+    }
+
+    localStorage.setItem('issues', JSON.stringify(issues))
+
+    fetchIssues()
+}
+
+function deleteIssue (id) {
+    let issues = JSON.parse(localStorage.getItem('issues'))
+    for(let i=0; i < issues.length; i++) {
+        if(issues[i].id === id) {
+            issues.splice(i,1)
+        }
+    }
+
+    localStorage.setItem('issues', JSON.stringify(issues))
+
+    fetchIssues()
+
 }
